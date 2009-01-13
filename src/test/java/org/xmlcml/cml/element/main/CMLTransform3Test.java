ad776789cf658aece274d6feb55f1e0dde801344
petermr <petermr@localhost>
2009-01-13 10:58:20 +0000

    additional import static refactor

--- src/test/java/org/xmlcml/cml/element/main/CMLTransform3Test.java
+++ src/test/java/org/xmlcml/cml/element/main/CMLTransform3Test.java
@@ -1,7 +1,7 @@
 package org.xmlcml.cml.element.main;
 
-import static org.xmlcml.euclid.EC.EPS;
-import static org.xmlcml.euclid.EC.S_RBRAK;
+import static org.xmlcml.euclid.EuclidConstants.EPS;
+import static org.xmlcml.euclid.EuclidConstants.S_RBRAK;
 import static org.xmlcml.euclid.test.EuclidTestBase.neverThrow;
 
 import java.io.IOException;
